2f44d2f623c703115e1be522b991e2226ec4c2a1
randa-11295 <randa.mohamed1295@gmail.com>
2022-06-29 20:53:41 +0200

    route

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,37 +1,46 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import SideBar from '../SideBar/SideBar';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import SideBar from "../SideBar/SideBar";
 import AddMovie from "../Movie/AddMovie";
-import AddActors from '../Actors/AddActors';
+import AddActors from "../Actors/AddActors";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const boxStyle = { flexGrow: 1, p: 5,  
-                   background : "#fff"  ,
-                   borderRadius : "15px" ,
-                   height : "100%" ,
-                 
-                   margin : "auto" , 
-                   boxShadow: "0px 0px 15px 0px rgba(0,0,0,0.12)"}
+const boxStyle = {
+  flexGrow: 1,
+  p: 5,
+  background: "#fff",
+  borderRadius: "15px",
+  height: "100%",
+  margin: "auto",
+  boxShadow: "0px 0px 15px 0px rgba(0,0,0,0.12)",
+};
 
- const continStyle ={ display: 'flex'  ,  px: 10  ,  py: 5 , 
-                      background : "rgb(238, 238, 238)" ,
-                      minHeight : "100vh" ,
-                      display : "flex" ,
-                      justifyContent : "center"  ,
-                     alignItems : "center" ,
-                    }                  
-                
+const continStyle = {
+  display: "flex",
+  px: 10,
+  py: 5,
+  background: "rgb(238, 238, 238)",
+  minHeight: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
 
 export default function App() {
   return (
-    <Box sx={continStyle}> 
+        <BrowserRouter>
+    <Box sx={continStyle}>
       <SideBar />
-    
+
       <Box component="main" sx={boxStyle}>
-
-            <AddActors />
-
-         {/* <AddMovie /> */}
+          <Routes>
+            <Route path="/" element={<AddMovie />} />
+            <Route path="/add-media" element={<AddMovie />} />
+            <Route path="/add-actor" element={<AddActors />} />
+          </Routes>
       </Box>
     </Box>
+        </BrowserRouter>
   );
 }
--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,51 +1,48 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import { Link } from "react-router-dom";
 
+const boxStyle = {
+  overflow: "auto",
+  background: "linear-gradient(20deg,#c33d70 0%, #7a3f85 85%)",
+  minHeight: "100vh",
+  width: "250px",
+  color: "#000",
+};
 
-const SideBar =()=>{
-return (
+const links = [{ route :"add-media" , label : "add media"}, {route :"add-actor" , label : "add actor"} ];
+
+const SideBar = () => {
+  return (
     <Drawer
-    variant="permanent"
-    sx={{
-      width: 250,
-      flexShrink: 0,
-    }}
-  >
-    <Box sx={{ overflow: 'auto'  ,
-    background: "linear-gradient(20deg,#c33d70 0%, #7a3f85 85%)",
-     minHeight : "100vh" , width : "250px"  , color : "#000" }}>
-      <List>
+      variant="permanent"
+      sx={{
+        width: 250,
+        flexShrink: 0,
+      }}
+    >
+      <Box sx={boxStyle}>
+        <List>
+          {links.map((text) => (
+            <ListItem key={text.route} disablePadding>
+              <ListItemButton  sx={{ display: "flex", justifyContent: "center" }}  >
+                <Link to={text.route}>
+                  <ListItemText
+                    sx={{ color: "#fff", textAlign: "center" }}
+                    primary={text.label}
+                  />
+                </Link>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Drawer>
+  );
+};
 
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemText primary={text} sx={{color : "#fff" , textAlign : "center"}} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemText primary={text} sx={{color : "#fff" , textAlign : "center"}} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  </Drawer>
-)
-}
-
-export default SideBar
+export default SideBar;
